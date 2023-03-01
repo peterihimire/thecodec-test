b357ccf3171be617c233f60e30ade98eb07bb00b
peterihimire <peterihimire@gmail.com>
2023-03-01 11:56:26 +0100

    Was able to write one test, in the case of empty field, return missing input field(s) error

--- src/__tests__/controllers/auth-controller.test.js
+++ src/__tests__/controllers/auth-controller.test.js
@@ -1,5 +1,39 @@
-const { register } = require('../../controllers/auth-controller')
+const { register } = require("../../controllers/auth-controller");
 
-describe('Register Route', () => {
-  
-})
+const db = require("../../models");
+const User = db.User;
+
+const mockRequest = (name, email, password) => {
+  return {
+    body: {
+      name: name,
+      email: email,
+      password: password,
+    },
+  };
+};
+
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+const mockNext = jest.fn();
+
+describe("Register Route", () => {
+  it("Should fail when input field(s) is missing ", async () => {
+    expect.assertions(2);
+
+    const res = mockResponse();
+    const req = mockRequest();
+    const next = mockNext;
+
+    await register(req, res, next);
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(next).toHaveBeenCalledWith(
+      Error("Input missing required field(s).")
+    );
+  });
+});
